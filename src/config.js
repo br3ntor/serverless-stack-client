4746e5aceb147e2e62748e51b9872b2bf40322fe
br3ntor <br3ntor@gmail.com>
2020-01-09 16:21:10 -0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: 'us-west-1',
     BUCKET: 'notes-app-uploads-a'
@@ -12,5 +13,6 @@ export default {
     USER_POOL_ID: 'us-west-2_eQaUIgEge',
     APP_CLIENT_ID: 'r8rmbrsmcg8qg0lguacv0upir',
     IDENTITY_POOL_ID: 'us-west-2:8e965f62-390c-4631-b900-7479a6803aec'
-  }
+  },
+  STRIPE_KEY: 'pk_test_xoYD3yD6l24JZR2upcYJbIQD00kn6CITAs'
 };
